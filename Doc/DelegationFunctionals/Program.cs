3f27b62e9b9fec9dfa74ec80b193523292f3623d
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-11-22 15:10:34 +0300

    Delegat end

---
+++ Doc/DelegationFunctionals/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegationFunctionals
+{
+    delegate double Function(double x);
+    class Multiplier 
+    {
+        double _factor;
+        public Multiplier(double factor) => _factor = factor;
+        public double Multiply(double x) => x * _factor;
+    }
+    internal class Program
+    {
+        static double[] Apply(double[] a, Function f)
+        {
+            var result = new double[a.Length];
+            for(int i = 0; i < a.Length; i++) result[i] = f(a[i]);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double[] a = { 0.0, 0.5, 1.0 };
+            double[] squares = Apply(a, (x) => x * x);
+            Multiplier m = new Multiplier(2.0);
+            double[] doubles = Apply(a, m.Multiply);
+            double[] sins = Apply(a, Math.Sin);
+
+            foreach (var item in a) Console.WriteLine($"a=" + item);
+            foreach (var item in squares) Console.WriteLine($"squares=" + item);
+            foreach (var item in doubles) Console.WriteLine($"doubles=" + item);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Doc/MainAreas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAreas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] a = new int[10];
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = i * i;
+                }
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Console.WriteLine($"a[{i}] = {a[i]}");
+                }
+
+                int[] a1 = new int[10];
+                int[,] a2 = new int[10, 5];
+                int[,,] a3 = new int[10, 5, 2];
+
+                int[][] b = new int[3][];
+                b[0] = new int[10];
+                b[1] = new int[5];
+                b[2] = new int[20];
+
+                int[] c = new int[] { 1, 2, 3 };
+                foreach (int item in c)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
