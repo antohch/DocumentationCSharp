b35b76dc5154ca7731f6cb87fbef4667864ebbc0
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-11-23 17:04:57 +0300

    Asyn end

--- Doc/AsyncAndAwait/Program.cs
+++ Doc/AsyncAndAwait/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,29 @@ namespace AsyncAndAwait
 {
     internal class Program
     {
+        static async Task<int> RetrieveDocsHomePage()
+        {
+            var client = new HttpClient();
+            byte[] content = await client.GetByteArrayAsync("https://learn.microsoft.com/");
+
+            Console.WriteLine($"{nameof(RetrieveDocsHomePage)}: Finished dlownloading.");
+
+            File.WriteAllBytes("sit.html", content);
+
+            return content.Length;
+        }
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            var i = RetrieveDocsHomePage();
+            while(true)
+            {
+                Console.WriteLine("true"+ rnd.Next(0, 100));
+            }
+
+            Console.WriteLine(i.Result);
+
+            Console.Read();
         }
     }
 }
