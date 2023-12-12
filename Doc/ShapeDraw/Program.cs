a829dfef1f632b0728349ee61c20c4bdf1c4d368
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-12-12 21:47:02 +0300

    Draw Polimorphism

---
+++ Doc/ShapeDraw/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeDraw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>
+            {
+                new Rectangle(),
+                new Triangle(),
+                new Circle()
+            };
+            foreach(var shape in shapes)
+            {
+                shape.Draw();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Doc/ShapeDraw/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeDraw
+{
+    internal class Shape
+    {
+        public int x { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+}
