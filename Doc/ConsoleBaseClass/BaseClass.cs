1331eb54b1ba879c523e2ca045ad129ffb8f9e70
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-12-13 13:06:50 +0300

    override sealed

---
+++ Doc/ConsoleBaseClass/BaseClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBaseClass
+{
+    public class BaseClass
+    {
+        public void DoWork() { WorkField++; }
+        public int WorkField;
+        public int WorkProperty
+        {
+            get { return 0; }
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        public new void DoWork() { WorkField++; }
+        public new int WorkField;
+        public new int WorkProperty
+        {
+            get { return 0; }
+        }
+    }
+
+    public class A
+    {
+        public virtual void DoWork() { }
+    }
+    public class B : A
+    {
+        public override void DoWork() { }
+    }
+    public class C : B
+    {
+        public sealed override void DoWork() { }
+    }
+    public class D : C
+    {
+        public new void DoWork() { }
+    }
+
+    public class Base
+    {
+        public virtual void DoWork() { }
+    }
+    public class Derived : Base
+    {
+        public override void DoWork() 
+        { 
+            base.DoWork();
+        }
+    }
+}
---
+++ Doc/ConsoleBaseClass/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleBaseClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass B = new DerivedClass();
+            B.DoWork();
+
+            BaseClass A = (BaseClass)B;
+            A.DoWork();
+
+            B b = new B();
+            b.DoWork();
+            C c = new C();
+            c.DoWork();
+            D d = new D();
+            d.DoWork();
+
+            Derived di = new Derived();
+            di.DoWork();
+        }
+    }
+}
