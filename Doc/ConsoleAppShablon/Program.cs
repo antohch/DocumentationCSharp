1775c25f0962be0e95648e6ca01b7b208e39e97f
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-12-16 15:14:14 +0300

    is

---
+++ Doc/ConsoleAppShablon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppShablon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? maybe = null;
+
+            if (maybe is int number)
+            {
+                Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+            }
+            else
+            {
+                Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+            }
+     
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Doc/ConsoleAppShablonNet/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleAppShablonNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? message = "Message";
+            if (message is null)
+            {
+                Console.WriteLine(message);
+            }
+            List<string> list = new List<string>();
+            list.Add("Message");
+
+            Console.WriteLine(MidPoint(list));
+        }
+        public static T MidPoint<T>(IEnumerable<T> sequence) 
+        { 
+            if (sequence is IList<T> list)
+            {
+                return list[list.Count / 2];
+            }
+            else if (sequence is null)
+            {
+                throw new ArgumentNullException(nameof(sequence), "Sequence can't be null.");
+            }
+            else
+            {
+                int halfLength = sequence.Count() / 2 - 1;
+                if (halfLength < 0) 
+                {
+                    halfLength = 0;
+                }
+                return sequence.Skip(halfLength).First();
+            }
+        }
+    }
+}
