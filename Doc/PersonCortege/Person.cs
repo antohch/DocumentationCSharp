1c49f09b08186ac97bdcd35f1307990994ed4710
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-12-24 19:26:54 +0300

    Deconstruct

---
+++ Doc/PersonCortege/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonCortege
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }  
+        public Person(string fname, string mname, string lname, string cityName, string stateName)
+        {
+            FirstName = fname;
+            LastName = lname;
+            MiddleName = mname;
+            City = cityName;
+            State = stateName;
+        }
+        public void Deconstruct(out string fname, out string lname)
+        {
+            fname = FirstName; 
+            lname = LastName;
+        }
+        public void Deconstruct(out string fname, out string mname, out string lname)
+        {
+            fname = FirstName;
+            mname = MiddleName;
+            lname = LastName;
+        }
+        public void Deconstruct(out string fname, out string lname, out string city, out string state)
+        {
+            fname = FirstName;
+            lname = LastName;
+            city = City;
+            state = State;
+        }
+    }
+}
---
+++ Doc/PersonCortege/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonCortege
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("John", "Quincy", "Admas", "Boston", "MA");
+            var (fName, _, city, _) = p;
+            Console.WriteLine($"Hello {fName} of {city}");
+        }
+    }
+}
