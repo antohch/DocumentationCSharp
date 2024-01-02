74f1d3fe125539a0952c779588cedd063c352952
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-02 18:08:14 +0300

    Элементы кортежа с dis карта

---
+++ Doc/QueryCityDataConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace QueryCityDataConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var (city, pop, size) = QueryCityData("New York City");
+
+        }
+        private static (string, int, double) QueryCityData(string name)
+        {
+            if (name == "New York City")
+                return (name, 8175133, 468.48);
+            return ("", 0, 0);
+        }
+    }
+}
