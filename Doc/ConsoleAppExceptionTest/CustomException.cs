b617f0254c2f9ccb23a89896b95033f4107a6ac9
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-17 13:09:19 +0300

    Обработка исключений

---
+++ Doc/ConsoleAppExceptionTest/CustomException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExceptionTest
+{
+    internal class CustomException : Exception
+    {
+        public CustomException(string message) { }
+    }
+}
--- Doc/ConsoleAppExceptionTest/Program.cs
+++ Doc/ConsoleAppExceptionTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,35 @@ namespace ConsoleAppExceptionTest
 {
     internal class Program
     {
+        static void TestFinally()
+        {
+            FileStream file = null;
+            FileInfo fileInfo = new System.IO.FileInfo("./file.txt");
+            try
+            {
+                file = fileInfo.OpenWrite();
+                file.WriteByte(0xF);
+            }
+            finally
+            {
+                file?.Close();
+            }
+
+            try
+            {
+                file = fileInfo.OpenWrite();
+                Console.WriteLine("OpenWrite() succeeded");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("OpenWrite() failed");
+            }
+
+        }
+        private static void TestThrow()
+        {
+            throw new CustomException("Custom exception in TestThrow()");
+        }
         static double SafeDivision(double x, double y)
         {
             if (y==0)
@@ -19,6 +49,15 @@ namespace ConsoleAppExceptionTest
         }
         static void Main(string[] args)
         {
+            TestFinally();
+            try
+            {
+                TestThrow();
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
             double a = 98, b = 0;
             double result;
             try
@@ -30,6 +69,28 @@ namespace ConsoleAppExceptionTest
             {
                 Console.WriteLine("Attempted divide by zero.");
             }
+
+            try
+            {
+                using (var sw = new StreamWriter("./test.txt"))
+                {
+                    sw.WriteLine("Hello");
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("Done");
+
             Console.ReadKey();
         }
     }
