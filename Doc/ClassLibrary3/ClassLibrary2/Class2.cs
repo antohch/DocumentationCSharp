d859822d17dcd34db45bad8acbdf2d15cfc3f233
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-20 21:55:56 +0300

    assembly

---
+++ Doc/ClassLibrary3/ClassLibrary2/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public int c;
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+        public static int GenerateRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+    }
+}
---
+++ Doc/ClassLibrary3/ClassLibrary3/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary3
+{
+    public class Class1
+    {
+        public int c;
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+        public static int GenerateRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+    }
+}
