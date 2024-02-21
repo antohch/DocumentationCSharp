0a66bab4fcf91fec986d1f327baf92d06cec7ca8
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-02-21 17:00:44 +0300

    Пример работы BankAccount

---
+++ Doc/Bank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Toha", 1000);
+            Console.WriteLine($"Account {account.Number} was create for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid ma back");
+            Console.WriteLine(account.Balance);
+
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                //return;
+            }
+
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            Console.ReadKey();
+        }
+    }
+}
