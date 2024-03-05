1e038ccedb9b9526f7835e2ddffcf2dda61f42dc
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-03-05 16:36:10 +0300

    Book

--- Doc/InheritanceMagazine/Program.cs
+++ Doc/InheritanceMagazine/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace InheritanceMagazine
+﻿
+namespace InheritanceMagazine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //var publication = new Publication("Tiddlywinks for Expers", "Fun and Games", PublicationType.Book);
+            var book = new Book("the Tempest", "0971655819", "Shakespeare, William", "Public Domain Press");
+            ShowPublicationInfo(book);
+        }
+
+        private static void ShowPublicationInfo(Publication pub)
+        {
+            string pubDate = pub.GetPublicationDate();
+            Console.WriteLine($"{pub.Title}, "+
+                $"{(pubDate == "NYP" ? "Not Yet Published" : "published on " + pubDate):d} by {pub.Publisher}");
         }
     }
 }
