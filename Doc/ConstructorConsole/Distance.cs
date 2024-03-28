bb49658cacf52c42ac2e6d6cb570a62b17fefc6e
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-03-28 16:39:40 +0300

    Конструкторы

---
+++ Doc/ConstructorConsole/Distance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorConsole
+{
+    public struct Distance(double dx, double dy)
+    {
+        public readonly double Magnitude => Math.Sqrt(dx * dx + dy * dy);
+        public readonly double Direction => Math.Atan2(dy, dx);
+
+        public void Translate(double deltaX, double deltaY)
+        {
+            dx += deltaX;
+            dy += deltaY;
+        }
+
+        public Distance() : this(0, 0) { }
+    }
+}
---
+++ Doc/Feature12/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Feature12
+{
+    public class C0
+    {
+        public void f(ref dynamic a)
+        {
+
+        }
+        public void M(dynamic d)
+        {
+            f(d);
+        }
+    }
+
+    class C
+    {
+        public string M(in int i) => "C";
+    }
+
+    static class E
+    {
+        public static string M(this C c, ref int i) => "E";
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i1 = 5;
+            Console.WriteLine(new C().M(ref i1));
+            Console.WriteLine(E.M(new C(), ref i1));
+
+            int[] a = [1, 2, 3, 4, 5, 6];
+            List<string> b = ["one", "two", "three"];
+            Span<char> c = ['a', 'b', 'c', 'd'];
+            int[][] twoD = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+            int[] row0 = [1, 2, 3];
+            int[] row1 = [4, 5, 6];
+            int[] row2 = [7, 8, 9];
+            int[][] twoDFromVariables = [row0, row1, row2];
+
+            int[] single = [.. row0, .. row1, .. row2];
+            foreach(var element in single)
+            {
+                Console.WriteLine($"{element}");
+            }
+
+            var buffer = new Buffer();
+            for (int i = 0; i < 10; i++)
+            {
+                buffer[i] = i;
+            }
+            foreach(var i in buffer)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        [System.Runtime.CompilerServices.InlineArray(10)]
+        public struct Buffer
+        {
+            private int _element0;
+        }
+
+    }
+}
