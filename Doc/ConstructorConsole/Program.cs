cd0b48b36c98b7c5ee454df1520c8baa5b211559
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-02 17:07:11 +0300

    Шаблоны

--- Doc/ConstructorConsole/Program.cs
+++ Doc/ConstructorConsole/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ConstructorConsole
+﻿using System.Numerics;
+
+namespace ConstructorConsole
 {
+    public interface IGetNext<T> where T : IGetNext<T>
+    {
+        static abstract T operator ++(T other);
+    }
     internal class Program
     {
+
+        public static double MidPoint(double left, double right) =>
+            (left + right) / (2.0);
+
+        public static T MidPoint<T>(T left, T right)
+            where T : INumber<T> => (left + right) / T.CreateChecked(2);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var str = new RepeatSequence();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(str++);
+            }
         }
     }
 }
---
+++ Doc/ConstructorConsole/RepeatSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorConsole
+{
+    public struct RepeatSequence : IGetNext<RepeatSequence>
+    {
+        private const char Ch = 'A';
+        public string Text = new string(Ch, 1);
+
+        public RepeatSequence() { }
+
+        public static RepeatSequence operator ++(RepeatSequence other)
+            => other with { Text = other.Text + Ch };
+
+        public override string ToString() => Text;
+    }
+}
