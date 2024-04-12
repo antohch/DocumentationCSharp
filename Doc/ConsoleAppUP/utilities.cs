e351c9151dcbacf58564898938c5e06e1c691473
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-12 15:12:20 +0300

    Analys

---
+++ Doc/ConsoleAppUP/utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUP
+{
+    public static class Utilities
+    {
+        public static async Task ShowConsoleAnimation(string[] animations)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                foreach (string s in animations)
+                {
+                    Console.Write(s);
+                    await Task.Delay(50);
+                    Console.Write("\b\b\b");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
