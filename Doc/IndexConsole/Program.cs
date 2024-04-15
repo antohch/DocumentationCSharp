ac694741b19fc6598961b66a717872f26b92e14d
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-15 12:34:05 +0300

    Index - array [..]

---
+++ Doc/IndexConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = [
+                // index from start    index from end
+    "The",      // 0                   ^9
+    "quick",    // 1                   ^8
+    "brown",    // 2                   ^7
+    "fox",      // 3                   ^6
+    "jumps",    // 4                   ^5
+    "over",     // 5                   ^4
+    "the",      // 6                   ^3
+    "lazy",     // 7                   ^2
+    "dog"       // 8                   ^1
+]; 
+        }
+    }
+}
