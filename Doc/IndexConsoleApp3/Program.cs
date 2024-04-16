a4fe17d037ebd2a968625738d56aafb11ed68cbe
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-16 14:54:15 +0300

    Сценарий для индексов

--- Doc/IndexConsoleApp3/Program.cs
+++ Doc/IndexConsoleApp3/Program.cs
@@ -78,6 +78,105 @@
             Span<int> z_end = numbers[z..];
             Span<int> z_zero = numbers[z..^0];
             Console.WriteLine($"\t{z_end[0]}..{z_end[^1]} is the same as {z_zero[0]}..{z_zero[^1]}");
+
+            Range implicitRange = 3..^5;
+
+            Range explicitRange = new(
+              new Index(3, false),
+              new Index( 5, true));
+            if (implicitRange.Equals(explicitRange))
+            {
+                Console.WriteLine($"{implicitRange} - {explicitRange}");
+            }
+
+            int[][] jagged =
+[
+   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+   [10,11,12,13,14,15,16,17,18,19],
+   [20,21,22,23,24,25,26,27,28,29],
+   [30,31,32,33,34,35,36,37,38,39],
+   [40,41,42,43,44,45,46,47,48,49],
+   [50,51,52,53,54,55,56,57,58,59],
+   [60,61,62,63,64,65,66,67,68,69],
+   [70,71,72,73,74,75,76,77,78,79],
+   [80,81,82,83,84,85,86,87,88,89],
+   [90,91,92,93,94,95,96,97,98,99],
+];
+
+            var selectedRows = jagged[3..^3];
+
+            foreach (var row in selectedRows)
+            {
+                var selectedColumns = row[2..^2];
+                foreach (var cell in selectedColumns)
+                {
+                    Console.Write($"{cell}, ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            int[] sequence = Sequence(1000);
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = start..(start + 10);
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:    \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = ^(start + 10)..^start;
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:  \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+
+            (int min, int max, double average) MovingAverage(int[] subSequence, Range range) =>
+                (
+                    subSequence[range].Min(),
+                    subSequence[range].Max(),
+                    subSequence[range].Average()
+                );
+
+            int[] Sequence(int count) => [.. Enumerable.Range(0, count).Select(x => (int)(Math.Sqrt(x) * 100))];
+
+
+
+
+
+
+            string[] fruits = { "apple", "banana", "mango", "orange",
+                      "passionfruit", "grape" };
+
+            var query =
+                fruits.Select((fruit, index) =>
+                                  new { index, str = fruit.Substring(0, index) });
+            IEnumerable<string> queryFruits = fruits.Select((fruit, index) => fruit + " - " + index);
+
+            foreach (var obj in query)
+            {
+                Console.WriteLine("{0}", obj);
+            }
+
+            /*
+             This code produces the following output:
+
+             { index = 0, str =  }
+             { index = 1, str = b }
+             { index = 2, str = ma }
+             { index = 3, str = ora }
+             { index = 4, str = pass }
+             { index = 5, str = grape }
+            */
+
+            var squares = Enumerable.Range(1, 10).Select(x => x * x);
+            foreach(int num in squares)
+            {
+                Console.WriteLine(num);
+            }
+
         }
     }
 }
