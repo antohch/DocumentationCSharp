bbbe6563148e8efaecb58bb57cafc09a73bd8225
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-17 17:29:40 +0300

    kata

--- Doc/KataTwoSum2/Kata.cs
+++ Doc/KataTwoSum2/Kata.cs
@@ -22,4 +22,69 @@ namespace KataTwoSum2
             return new int[0];
         }
     }
+    public class WeightSort
+    {
+
+        public static string orderWeight(string strng)
+        {
+            // your code
+            string[] weight = strng.Split(' ');
+            int[] ints = new int[weight.Length];
+            for (int i = 0; i < weight.Length; i++)
+            {
+                ints[i] = Int32.Parse(weight[i]);
+            }
+            Array.Reverse(ints);
+            for (int i = 0; i < ints.Length; i++)
+            {
+
+
+                for (int j = i + 1; j < ints.Length; j++)
+                {
+                    int sum1 = 0;
+
+                    string strItem = ints[i].ToString();
+                    char[] chars = strItem.ToCharArray();
+                    for (int k = 0; k < chars.Length; k++)
+                    {
+                        sum1 += Int32.Parse(chars[k].ToString());
+                    }
+
+
+                    int sum2 = 0;
+
+                    strItem = ints[j].ToString();
+                    chars = strItem.ToCharArray();
+                    for (int k = 0; k < chars.Length; k++)
+                    {
+                        sum2 += Int32.Parse(chars[k].ToString());
+                    }
+
+                    if (sum1 > sum2)
+                    {
+                        int tmp = ints[i];
+                        ints[i] = ints[j];
+                        ints[j] = tmp;
+                    }
+                    else if (sum1 == sum2)
+                    {
+                        if (ints[i] > ints[j])
+                        {
+                            int tmp = ints[i];
+                            ints[i] = ints[j];
+                            ints[j] = tmp;
+                        }
+                    }
+
+                }
+
+            }
+            for (int i = 0; i < ints.Length; i++)
+            {
+                weight[i] = ints[i].ToString();
+            }
+            return String.Join(' ', weight);
+        }
+    }
 }
+
--- Doc/KataTwoSum2/Program.cs
+++ Doc/KataTwoSum2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Kata.TwoSum(new[] { 1, 2, 3 }, 4).OrderBy(a => a).ToArray());
+            WeightSort.orderWeight("2000 103 123 4444 99");
         }
     }
 }
