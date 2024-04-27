45ceca8fae9ff67a53aa20e4427460295a72c3d5
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-27 16:12:57 +0300

    Concat

---
+++ Doc/Linq_Chunk/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Chunk
+{
+    internal class Program
+    {
+        class NameList
+        {
+            public string[] FirstName;
+            public string[] LastName;
+        }
+        static void Main(string[] args)
+        {
+            NameList nameList = new NameList();
+            nameList.FirstName = new string[]{ "one", "two", "three", "five" };
+            nameList.LastName = new string[]{ "LastNameone", "LastNametwo", "LastNamethree", "LastNamefive" };
+
+            IEnumerable<string> query = nameList.FirstName.Concat(nameList.LastName.Select(x => x.ToString()));
+        }
+    }
+}
---
+++ Doc/Linq_Concat/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Linq_Concat
+{
+    internal class Program
+    {
+        class Pet
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        static Pet[] GetCats()
+        {
+            Pet[] cats = { new Pet { Name="Barley", Age=8 },
+                   new Pet { Name="Boots", Age=4 },
+                   new Pet { Name="Whiskers", Age=1 } };
+            return cats;
+        }
+
+        static Pet[] GetDogs()
+        {
+            Pet[] dogs = { new Pet { Name="Bounder", Age=3 },
+                   new Pet { Name="Snoopy", Age=14 },
+                   new Pet { Name="Fido", Age=9 } };
+            return dogs;
+        }
+
+        public static void ConcatEx1()
+        {
+            Pet[] cats = GetCats();
+            Pet[] dogs = GetDogs();
+
+            IEnumerable<string> query = cats.Select(cat => cat.Name).Concat(dogs.Select(dog => dog.Name));
+
+            foreach (string name in query)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ConcatEx1();
+
+            Pet[] cats = GetCats();
+            Pet[] dogs = GetDogs();
+
+            IEnumerable<string> query =
+                new[] { cats.Select(cat => cat.Name), dogs.Select(dog => dog.Name) }
+                .SelectMany(name => name);
+
+            foreach (string name in query)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
