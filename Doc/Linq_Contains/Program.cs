bc098ecbb3188d9e0e2e5b300c0fa82bcf458e80
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-05-02 13:16:16 +0300

    Contains

---
+++ Doc/Linq_Contains/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Linq_Contains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apple", "banana", "mango", "orange", "passionfruit", "grape" };
+            string fruit = "mango";
+
+            bool hasMango = fruits.Contains(fruit);
+            Console.WriteLine("The array {0} contain '{1}'.", hasMango ? "does": "does not", fruit);
+
+            Console.ReadKey();
+        }
+    }
+}
